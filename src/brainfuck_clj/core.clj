5cf3cca847931e8311516aa7f1f8acf4d2ca1ec8
Bobbus Mustelidus <inhortte@gmail.com>
2016-01-22 22:43:44 +0100

    my leper hurts

--- src/brainfuck_clj/core.clj
+++ src/brainfuck_clj/core.clj
@@ -4,6 +4,11 @@
 (def max-size 30000)
 (def memoria (atom (byte-array (repeat max-size (byte 0)))))
 
+(defn b-inc [v] (byte (if (= v (byte 127)) -128 (inc v))))
+(defn b-dec [v] (byte (if (= v (byte -128)) 127 (dec v))))
+(defn b-out [v] (char (if (< v 0) (+ v 256) v)))
+(defn c-in [c] (let [v (int c)] (byte (if (> v 127) (- v 256) v))))
+
 ;; {:pointer 0 :program []}
 
 (defn extract-loop [state]
@@ -19,10 +24,37 @@
     (assoc state :pointer (inc pointer) :program (rest program))
     (throw (Error. "Reaching beyond the end of the brainfuck universe"))))
 
+(defn dec-ptr [{:keys [pointer program] :as state}]
+  (if (> pointer 0)
+	(assoc state :pointer (dec pointer) :program (rest program))
+    (throw (Error. "Stumbling backwards over zero"))))
+
+(defn alter-cell [f pointer]
+  (let [v (aget @memoria pointer)]
+    (reset! memoria 
+      (do (aset-byte @memoria pointer (f v))
+          @memoria))))
+
+(defn inc-cell [{:keys [pointer program] :as state}]
+  (alter-cell b-inc pointer)
+  (assoc state :program (rest program)))
+  
+(defn dec-cell [{:keys [pointer program] :as state}]
+  (alter-cell b-dec pointer)
+  (assoc state :program (rest program)))
+
 (defn dispatch [{:keys [pointer program] :as state}]
   (let [[c & r] program]
     (case c
-      \> (inc-ptr state))))
+      \> (inc-ptr state)
+      \< (dec-ptr state)
+      \+ (inc-cell state)
+      \- (dec-cell state)
+      \. (do (print (b-out (aget @memoria pointer)))
+            (flush)
+            (assoc state :program (rest program)))
+      \, (do (alter-cell (fn [_] (c-in (. System/in read))) pointer)
+            (assoc state :program (rest program))))))
 
 (defn interpret [code]
   (loop [{:keys [pointer program] :as state} {:pointer 0 :program code}]
